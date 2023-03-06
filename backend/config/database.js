8708958e69018555714cca47ac20199a0f94f654
MahmoudMetwall12 <101277680+MahmoudMetwall12@users.noreply.github.com>
2023-03-06 21:42:54 +0200

    DBOnline

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,15 +4,14 @@ mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", true);
 // Connect MongoDB at default port 27017.
 mongoose.connect(
-  process.env.DBURL,
-  {
-    useNewUrlParser: true,
-  },
-  (err) => {
-    if (!err) {
-      console.log("connect successfully to MongoDB.");
-    } else {
-      console.log(`Error in DB connection:  ${err} `);
+    process.env.DBURLONLINE, {
+        useNewUrlParser: true,
+    },
+    (err) => {
+        if (!err) {
+            console.log("connect successfully to MongoDB.");
+        } else {
+            console.log(`Error in DB connection:  ${err} `);
+        }
     }
-  }
 );
