95cbb8f9ad0ce18e4577d8ac3b6640aec26d5550
Mahmoud <alim46972@gmail.com>
2023-03-10 22:09:39 +0200

    Handling Mongoose Key errors and JWT errors

--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -14,10 +14,24 @@ module.exports = (err, req, res, next) => {
       const message = `Resource not found Invalid access to Resource :${err.path}`;
       error = new ErrorHandler(message, 400);
     }
+    //Handling Mongoose key Errors
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    }
 
     // Handling Mongoose Validation Error
     if (err.name == "ValidationError") {
       const message = Object.values(err.errors).map((value) => value.message);
+      const error = new ErrorHandler(message, 400);
+    }
+    //Handling wrong JWT error
+    if (err.name == "JsonWebTokenError") {
+      const message = `JSON Web Token is invalid please try agian..!!`;
+      const error = new ErrorHandler(message, 400);
+    }
+    //Handling Expired JWT error
+    if (err.name == "TokenExpiredError") {
+      const message = `JSON Web Token is expired please try agian..!!`;
       const error = new ErrorHandler(message, 400);
     }
 
