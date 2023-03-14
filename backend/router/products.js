0d89a8b7b841c9469ce248a308d1bd00878d4bf6
Mahmoud <alim46972@gmail.com>
2023-03-14 22:48:11 +0200

    Create the Product Reviews Controller and Router and modify the product schema from some mistake and update product rating after that.

--- backend/router/products.js
+++ backend/router/products.js
@@ -1,20 +1,22 @@
 const express = require("express");
 var router = express.Router();
 const {
-    getProducts,
-    NewProduct,
-    getSingleProducts,
-    UpdateSingleProduct,
-    deleteSingleProduct,
+  getProducts,
+  NewProduct,
+  getSingleProducts,
+  UpdateSingleProduct,
+  deleteSingleProduct,
+  createProductReview,
 } = require("../controller/products");
 const { auth, Roles } = require("../middleware/auth");
 
 router.route("/products").get(getProducts);
 router.route("/products/:id").get(getSingleProducts);
+router.route("/product/review").put(auth, createProductReview);
 router
-    .route("/admin/products/:id")
-    .put(auth, Roles("Admin"), UpdateSingleProduct)
-    .delete(auth, Roles("Admin"), deleteSingleProduct);
+  .route("/admin/products/:id")
+  .put(auth, Roles("Admin"), UpdateSingleProduct)
+  .delete(auth, Roles("Admin"), deleteSingleProduct);
 router.route("/admin/products/new").post(auth, Roles("Admin"), NewProduct);
 
 module.exports = router;
