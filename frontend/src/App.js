23d1197d0adbcd92ac51a0fd2723a41b43374a30
MahmoudMetwall12 <101277680+MahmoudMetwall12@users.noreply.github.com>
2023-03-29 16:18:12 +0200

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Header from "./components/layouts/header";
+import Footer from "./components/layouts/footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>ShopIT</h1>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
