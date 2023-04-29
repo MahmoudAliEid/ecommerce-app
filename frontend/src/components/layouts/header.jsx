ea6cf27f56167f70e46888a82e019b8f24eb146b
Mahmoud <alim46972@gmail.com>
2023-04-29 23:31:19 +0300

    Showe User Detials in the Header

--- frontend/src/components/layouts/header.jsx
+++ frontend/src/components/layouts/header.jsx
@@ -3,8 +3,9 @@ import '../../App.css';
 import Search from './search';
 import { Link } from 'react-router-dom';
 
+import { useDispatch, useSelector } from 'react-redux'
 const Header = () => {
-  
+   const { user, loading } = useSelector(state => state.auth)
   return (
     <Fragment>
       <nav className="navbar row">
@@ -21,17 +22,45 @@ const Header = () => {
 
 
         <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-          <Link to="/login"  className="btn ml-4" id="login_btn">
-            Login
-          </Link>
+                    <Link to="/cart" style={{ textDecoration: 'none' }} >
+                        <span id="cart" className="ml-3">Cart</span>
+                        <span className="ml-1" id="cart_count">10</span>
+                    </Link>
 
-          <span id="cart" className="ml-3">
-            Cart
-          </span>
-          <span className="ml-1" id="cart_count">
-            2
-          </span>
-        </div>
+                    {user ? (
+                        <div className="ml-4 dropdown d-inline">
+                            <Link to="#!" className="btn dropdown-toggle text-white mr-4" type="button" id="dropDownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                                <figure className="avatar avatar-nav">
+                                    <img
+                                        src={user.images && user.images.picture}
+                                        alt={user && user.name}
+                                        className="rounded-circle"
+                                    />
+                                </figure>
+                                <span>{user && user.name}</span>
+                            </Link>
+
+                            <div className="dropdown-menu" aria-labelledby="dropDownMenuButton">
+
+                                {user && user.role === 'admin' && (
+                                    <Link className="dropdown-item" to="/dashboard">Dashboard</Link>
+                                )}
+                                <Link className="dropdown-item" to="/orders/me">Orders</Link>
+                                <Link className="dropdown-item" to="/me">Profile</Link>
+                                <Link className="dropdown-item text-danger" to="/" >
+                                    Logout
+                                </Link>
+
+                            </div>
+
+
+                        </div>
+
+                    ) : !loading && <Link to="/login" className="btn ml-4" id="login_btn">Login</Link>}
+
+
+                </div>
       </nav>
     </Fragment>
   );
